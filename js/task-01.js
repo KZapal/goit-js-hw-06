f2ae23b4744a80a0d8d5762cbfe48e3eb7551df9
KZapal <kamil.zapal@o2.pl>
2023-10-11 23:05:14 +0200

    finish task 1 from hw6

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,12 @@
-const categories = document.querySelector("#categories");
-const NumbersOfCategories = categories.children.length;
-console.log(`Number of categories: ${NumbersOfCategories}`);
+const categoriesList = document.querySelector(`#categories`);
+const numbersOfCategories = categoriesList.children.length;
+console.log(`Number of categories: ${numbersOfCategories}`);
+
+let items = categoriesList.querySelectorAll(`.item`);
+
+for (const item of items) {
+  const headItem = item.firstElementChild;
+  const NumberOfItemsInItem = item.lastElementChild.children.length;
+  console.log(`Category: ${headItem.innerHTML}`);
+  console.log(`Elements: ${NumberOfItemsInItem}`);
+}
