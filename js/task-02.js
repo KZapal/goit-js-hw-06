392d66380261627ea4e1c9d3aadbe2585921b175
KZapal <kamil.zapal@o2.pl>
2023-10-11 23:49:15 +0200

    finish task2 from hw6

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector(`#ingredients`);
+
+const elementsList = ingredients
+  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+  .join("");
+
+ingredientsList.insertAdjacentHTML(`afterbegin`, elementsList);
