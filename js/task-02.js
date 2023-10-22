3f4217f25a81df07b3186b1439f301035829f096
KZapal <kamil.zapal@o2.pl>
2023-10-22 20:23:29 +0200

    a

--- js/task-02.js
+++ js/task-02.js
@@ -9,15 +9,7 @@ const ingredients = [
 
 const ingredientsList = document.querySelector(`#ingredients`);
 
-//(const elementsList = ingredients
-//.map((ingredient) => `<li class="item">${ingredient}</li>`)
-//.join("");
-
-//ingredientsList.insertAdjacentHTML(`afterbegin`, elementsList);)
-
-for (const ingredient of ingredients) {
-  const item = document.createElement(`li`);
-  item.textContent = ingredient;
-  item.classList.add(`item`);
-  ingredientsList.appendChild(item);
-}
+const item = document.createElement(`li`);
+item.innerText = ingredients[0];
+item.classList.add(`item`);
+ingredientsList.appendChild(item);
